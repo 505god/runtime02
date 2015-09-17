b479ec53f12ba0f349c25fc9ed73483e0844995a
qiu <18915410342@126.com>
2015-09-17 14:59:23 +0800

    Initial Commit

---
+++ runtime02/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  runtime02
+//
+//  Created by 邱成西 on 15/9/17.
+//  Copyright (c) 2015年 邱成西. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
